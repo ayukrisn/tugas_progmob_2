2d6c7ab2df85ca9d2ffc17ec9dba76deb4648765
ayukrisn <ayuukrisn@gmail.com>
2024-04-28 15:44:49 +0800

    done ngeliatin detail

--- lib/features/users/screens/user_detail.dart
+++ lib/features/users/screens/user_detail.dart
@@ -2,19 +2,98 @@ import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:dio/dio.dart';
 
-class UserDetail extends StatelessWidget {
+class UserDetail extends StatefulWidget {
   const UserDetail({super.key});
 
   @override
+  State<UserDetail> createState() => _UserDetailState();
+}
+
+class _UserDetailState extends State<UserDetail> {
+  Anggota? anggota;
+  int id = 0;
+  final _dio = Dio();
+  final _storage = GetStorage();
+  final _apiUrl = 'https://mobileapis.manpits.xyz/api';
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (ModalRoute.of(context)?.settings.arguments != null) {
+      id = ModalRoute.of(context)?.settings.arguments as int;
+      getDetail();
+    }
+  }
+
+  Future<void> getDetail() async {
+    try {
+      final _response = await _dio.get(
+        '${_apiUrl}/anggota/${id}',
+        options: Options(
+          headers: {'Authorization': 'Bearer ${_storage.read('token')}'},
+        ),
+      );
+      Map<String, dynamic> responseData = _response.data;
+      print(responseData);
+      print(id);
+      setState(() {
+        anggota = Anggota.fromJson(responseData);
+      });
+    } on DioException catch (e) {
+      print('${e.response} - ${e.response?.statusCode}');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  @override
   Widget build(BuildContext context) {
-    final int id = ModalRoute.of(context)!.settings.arguments as int;
-    
     return Scaffold(
+      backgroundColor: Color(0xFFFAFAFA),
       appBar: AppBar(
-        title: Text('Anggota Detail'),
+        title: Text('Anggota',
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  color: Color(0xFF5E5695),
+                )),
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color.fromARGB(26, 94, 86, 149),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add,
+                size: 32,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          SizedBox(width: 16),
+        ],
       ),
       body: Center(
-        child: Text('Anggota Name: ${id}'),
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 68),
+            child: anggota == null
+                ? Text("Belum ada anggota")
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Text("Anggota ID: ${anggota?.id}"),
+                        Text("Anggota No Induk: ${anggota?.nomor_induk}"),
+                        Text("Anggota Nama: ${anggota?.nama}"),
+                        Text("Anggota Alamat: ${anggota?.alamat}"),
+                        Text("Anggota Tanggal Lahir: ${anggota?.tgl_lahir}"),
+                        Text("Anggota Telepon: ${anggota?.telepon}"),
+                        Text("Anggota Image URL: ${anggota?.image_url}"),
+                        Text("Anggota Status Aktif: ${anggota?.status_aktif}"),
+                      ])),
       ),
     );
   }
@@ -42,15 +121,26 @@ class Anggota {
   });
 
   factory Anggota.fromJson(Map<String, dynamic> json) {
-    return Anggota(
-      id: json['id'],
-      nomor_induk: json['nomor_induk'],
-      nama: json['nama'],
-      alamat: json['alamat'],
-      tgl_lahir: json['tgl_lahir'],
-      telepon: json['telepon'],
-      image_url: json['image_url'],
-      status_aktif: json['status_aktif'],
-    );
+    final data = json['data'] as Map<String, dynamic>?;
+
+    if (data != null) {
+      final anggotaData = data['anggota'] as Map<String, dynamic>?;
+
+      if (anggotaData != null) {
+        return Anggota(
+          id: anggotaData['id'],
+          nomor_induk: anggotaData['nomor_induk'],
+          nama: anggotaData['nama'],
+          alamat: anggotaData['alamat'],
+          tgl_lahir: anggotaData['tgl_lahir'],
+          telepon: anggotaData['telepon'],
+          image_url: anggotaData['image_url'],
+          status_aktif: anggotaData['status_aktif'],
+        );
+      }
+    }
+
+    // If data or anggotaData is null, throw an exception or return a default instance
+    throw Exception('Failed to parse Anggota from JSON');
   }
 }
